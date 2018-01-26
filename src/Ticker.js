46cefe2e30ecf67d5996753cfdb8a8d1a19acb58
Dmitry Sky <dmitry.sky@icloud.com>
2018-01-26 13:04:47 -0700

    rebuild the ticker footer from the ground up

--- src/Ticker.js
+++ src/Ticker.js
@@ -4,14 +4,20 @@ import './Ticker.css'
 class Ticker extends Component {
   render() {
     return (
-      <div className="ticker-wrap">
-        <div className="ticker">
-          <div className="ticker__item">Letterpress chambray brunch.</div>
-          <div className="ticker__item">Vice mlkshk crucifix beard chillwave meditation hoodie asymmetrical Helvetica.</div>
-          <div className="ticker__item">Ugh PBR&B kale chips Echo Park.</div>
-          <div className="ticker__item">Gluten-free mumblecore chambray mixtape food truck. </div>
-          <div className="ticker__item">Authentic bitters seitan pug single-origin coffee whatever.</div>
-          <div className="ticker__item">Letterpress chambray brunch.</div>
+      <div class="ticker">
+        <div class="ticker-items">
+          <span class="ticker-item">
+            Bitcoin
+          </span>
+          <span class="ticker-item">
+            Ethereum
+          </span>
+          <span class="ticker-item">
+            DASH
+          </span>
+          <span class="ticker-item">
+            LTC
+          </span>
         </div>
       </div>
     );
